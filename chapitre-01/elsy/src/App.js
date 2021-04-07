490c0a09d970e471d42c8bbcee672e75132d476c
unknown <jadisnou@gmail.com>
2021-04-07 15:25:22 +0200

    suite

---
+++ chapitre-01/elsy/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './App.css';
+const tempMin = -20
+const tempMax = 40
+const heartMin = 80
+const heartMax = 180
+const stepMin = 0
+const stepMax = 50000
+
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
