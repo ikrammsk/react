85685e0d051caf41a1dda49765417e9355e1881f
Ikram <jadisnou@gmail.Com>
2021-04-29 18:14:05 +0200

    exos non finis

--- jour-5/countries/src/App.js
+++ jour-5/countries/src/App.js
@@ -14,7 +14,29 @@ class App extends React.Component {
             population: "",
             region: ""
         }
+
+        this.getCountry = this.getCountry.bind(this)
+        this.componentDidMount = this.componentDidMount.bind(this)
     }
+
+    getCountry(country) {
+        // console.log("https://restcountries.eu/rest/v2/name/"+ country)
+        fetch("https://restcountries.eu/rest/v2/name/" + country)
+            .then(res => res.json())
+            .then(result => {
+                console.log(result)
+
+                this.setState({
+                    name: result[0].name,
+                    capital: result[0].capital,
+                    flag: result[0].flag,
+                    population: result[0].population,
+                    region: result[0].region
+                })
+            })
+            .catch(error => console.error(error));
+    }
+
 
     componentDidMount() {
         fetch("https://restcountries.eu/rest/v2/name/france")
@@ -36,31 +58,11 @@ class App extends React.Component {
     }
 
 
-
-    getCountry(country) {
-        // console.log("https://restcountries.eu/rest/v2/name/"+ country)
-        fetch("https://restcountries.eu/rest/v2/name/"+ country)
-            .then(res => res.json())
-            .then(result => {
-                console.log(result)
-
-                this.setState({
-                    // name: result[0].name,
-                    // capital: result[0].capital,
-                    // flag: result[0].flag,
-                    // population: result[0].population,
-                    // region: result[0].region
-                })
-            })
-            .catch(error => console.error(error));
-    }
-
-
     render() {
 
         return (
 
-            <div>
+            <div class="list">
 
                 <Button onClick={this.getCountry}>France</Button>
                 <Button onClick={this.getCountry}>Brazil</Button>
@@ -68,10 +70,10 @@ class App extends React.Component {
 
                 <p> Name : {this.state.name}</p>
                 <p> Capital : {this.state.capital}</p>
-                <p> Flag : {this.state.flag}</p>
+                <p> Flag : <img className="img"src={this.state.flag}alt="drapeau fr"></img></p>
                 <p> Population : {this.state.population}</p>
                 <p> Region : {this.state.region}</p>
-            
+
 
             </div >
         )
--- jour-5/countries/src/components/Button.jsx
+++ jour-5/countries/src/components/Button.jsx
@@ -7,12 +7,10 @@ import React from "react";
 class Button extends React.Component {
 
     render() {
+
         const countryName = this.props.children
-        
         return (
-            <button type="button" class="btn btn-prymary"
-                onClick={()=> this.props.onClick(countryName)}>
-                {countryName}
+            <button type="button" class="btn btn-primary"onClick={()=> this.props.onClick(countryName)}>{countryName}
             </button>
         )
     }
